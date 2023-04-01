c087ede35180e630ce8e44d6ca2965f86d6c9c89
Solomon Olatunji <realsolomon07@gmail.com>
2023-04-01 20:59:49 +0100

    ...

--- components/Underline.tsx
+++
@@ -1,3 +0,0 @@
-export default function Underline({ children, color }: { children: React.ReactNode; color: string }) {
-    return <span className={`relative transition-filte duration-500 hover:saturate-200 ${color}`}>{children}</span>;
-}
